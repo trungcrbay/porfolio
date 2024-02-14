066539c8860a8a3e3faa2ad9ea3b55c16006a4a3
Nguyen Dang Trung <trungnguyenjr123@gmail.com>
2024-02-14 12:35:03 +0700

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import SplashScreen from "@/components/FullScreenLoader";
 import RandomFact from "@/components/fact";
 import Header from "@/components/header";
-import Info from "@/components/info";
 import Skills from "@/components/skills";
 
 export default function Home() {
